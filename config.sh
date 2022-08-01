9c9d7d461b5c2613ae1b6e119dd0dcc403e31859
Toni Hermoso Pulido <toniher@cau.cat>
2022-08-01 16:22:12 +0200

    update

--- config.sh
+++ config.sh
@@ -10,7 +10,7 @@ export TF_VAR_vpc_id=vpc-68c6c103
 export TF_VAR_key_name=key-nf
 export TF_VAR_ingress_cidr_blocks=0.0.0.0/0
 export TF_VAR_release_label=emr-5.16.0
-export TF_VAR_emr_applications='["Hadoop", "Spark"]'
+export TF_VAR_applications='["Hadoop", "Spark"]'
 
 # Master node configurations
 export TF_VAR_master_instance_type=m3.xlarge
