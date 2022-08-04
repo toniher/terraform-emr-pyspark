8016ad5eab8ed842b57165b34cbcc5ac4378e3e1
Toni Hermoso Pulido <toniher@cau.cat>
2022-08-04 09:39:27 +0200

    changed scripts

--- scripts/bootstrap_actions.sh
+++ scripts/bootstrap_actions.sh
@@ -1,21 +1,17 @@
 #!/usr/bin/env bash
 
 # install conda
-wget --quiet https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh -O ~/miniconda.sh \
-    && /bin/bash ~/miniconda.sh -b -p $HOME/conda
+# wget --quiet https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh -O ~/miniconda.sh \
+#    && /bin/bash ~/miniconda.sh -b -p $HOME/conda
 
-echo -e '\nexport PATH=$HOME/conda/bin:$PATH' >> $HOME/.bashrc && source $HOME/.bashrc
-
-conda config --set always_yes yes --set changeps1 no
-conda config -f --add channels conda-forge
-conda config -f --add channels defaults
+# echo -e '\nexport PATH=$HOME/conda/bin:$PATH' >> $HOME/.bashrc && source $HOME/.bashrc
 
 # install packages
-conda install -y pandas seaborn git
+# conda install -y pandas seaborn git
 
-#install findspark
-pip install --upgrade pip
-pip install findspark
-pip install sklearn
+sudo yum update -y
+sudo yum install -y jq git
+
+sudo python3 -m pip install boto3 botocore ec2-metadata findspark
 
 
--- scripts/pyspark_quick_setup.sh
+++ scripts/pyspark_quick_setup.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 # bind conda to spark
-echo -e "\nexport PYSPARK_PYTHON=/home/hadoop/conda/bin/python" >> /etc/spark/conf/spark-env.sh
+# echo -e "\nexport PYSPARK_PYTHON=/home/hadoop/conda/bin/python" >> /etc/spark/conf/spark-env.sh
 
 # If Jupyter is used
 #echo "export PYSPARK_DRIVER_PYTHON=/home/hadoop/conda/bin/jupyter" >> /etc/spark/conf/spark-env.sh
